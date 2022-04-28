2fe7766b790cae598aa73dc5cb5f5d6e3ad23aa6
shmuel harel <shmuelharel10@gmail.com>
2022-04-28 17:15:05 +0300

    commit_04
    I updated that it is not possible to press more than two buttons

--- memory_game_1.js
+++ memory_game_1.js
@@ -17,41 +17,36 @@ console.log(a);
 
 let flipCards = [];
 const chooseCard = (event) => {
-   event.target.classList.add("show",`${event.target.id}`);
-   // event.target.classList.add();
+   if((flipCards.length<2)&&(flipCards[1] !=flipCards[0].title)){
+   event.target.classList.add( `${event.target.id}`);
    flipCards.push(event.target);
    console.log(flipCards);
-   // debugger
    if (flipCards.length % 2 == 0) {
-  setTimeout(() => {
-      if (flipCards[0].id == flipCards[1].id) {
-         console.log("goooood");
-         flipCards[1].classList.add("hide")
-         flipCards[0].classList.add("hide")
-        
-      }
-      else {
-         console.log("bazzzzzz");
-         flipCards[0].classList.remove(`show`, `${flipCards[0].id}`)
-         flipCards[1].classList.remove(`show`, `${flipCards[1].id}`)
-      }
-       flipCards = []
-      }, (2*1000));
-   } 
-  
+      setTimeout(() => {
+         if (flipCards[0].id == flipCards[1].id) {
+            console.log("goooood");
+            flipCards[1].classList.add("hide")
+            flipCards[0].classList.add("hide")
+         }
+         else {
+            console.log("bazzzzzz");
+            flipCards[0].classList.remove(`show`, `${flipCards[0].id}`)
+            flipCards[1].classList.remove(`show`, `${flipCards[1].id}`)
+         }
+         flipCards = []
+      }, (2 * 1000));
+   }
 }
-
-
+}
+let title = 0
 let elem;
 let board = document.getElementById("game-table");
 for (i of a) {
    let elem = document.createElement("div");
-   elem.innerText = "" ;
    elem.id = i
-   console.log(elem.id);
-   elem.className = `card `;
+   elem.title = ++title;
+   elem.className = `card`;
    elem.onclick = chooseCard;
-   // Tag.className = "card card-hover"
    board.appendChild(elem)
 }
 // debugger
